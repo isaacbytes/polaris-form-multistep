6879fc8561b5a03eb082f23d2fa12b30ba3c6224
Isaac Farciert <rfisaac2296@yahoo.com>
2019-05-15 17:47:21 -0400

    Outline app skeleton

--- src/App.js
+++ src/App.js
@@ -1,24 +1,13 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/* Insert Master Form component to contain the following: */}
+      {/* Master Form Progress & Navigation */}
+      {/* Step View (this will contain the rendered form at the current step) */}
+      {/* Master Form Controls */}
     </div>
   );
 }
